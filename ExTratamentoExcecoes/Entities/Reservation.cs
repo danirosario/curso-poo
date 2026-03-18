24cd9db2598dc18b5db82dd2f2b3803389beedf4
dani <rosario.dani01@gmail.com>
2026-03-18 15:54:38 -0300

    implementar classe Reservation

---
+++ ExTratamentoExcecoes/Entities/Reservation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExTratamentoExcecoes.Entities
+{
+    internal class Reservation
+    {
+        int RoomNumber { get; set; }
+        DateTime Checkin { get; set; }
+        DateTime Checkout { get; set; }
+
+        public Reservation(int roomNumber, DateTime checkin , DateTime checkout)
+        {
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            if (checkin < DateTime.Now || checkout < DateTime.Now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+    }
+}
--- ExTratamentoExcecoes/Program.cs
+++ ExTratamentoExcecoes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ExTratamentoExcecoes.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
