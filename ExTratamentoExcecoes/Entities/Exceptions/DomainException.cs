06fffee9426bcb57aee599edfaebe36f7bddf498
dani <rosario.dani01@gmail.com>
2026-03-25 20:43:14 -0300

    finalizando atividade sobre tratamento de exceções;

--- ExTratamentoExcecoes/Entities/Exceptions/DomainException.cs
+++ ExTratamentoExcecoes/Entities/Exceptions/DomainException.cs
@@ -4,10 +4,7 @@ namespace ExTratamentoExcecoes.Entities.Exceptions
 {
     internal class DomainException : ApplicationException
     {
-        public DomainException() { }
-
         public DomainException(string message) : base(message) { }
 
-        public DomainException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
