5e595c7abae55a3f4976c2dc936f344f9911e076
dani <rosario.dani01@gmail.com>
2026-04-03 12:06:51 -0300

    Implement contract installment payment system

--- ExFixInterface/Program.cs
+++ ExFixInterface/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using ExFixInterface.Entities;
+using ExFixInterface.Services;
+using System;
 namespace ExFixInterface
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date: (dd/MM/yyyy):");
+            DateTime contractDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine());
+            Console.Write("Enter number of installments: ");
+            int numberOfInstallments = int.Parse(Console.ReadLine());
+
+            Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, numberOfInstallments);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+
+            }
         }
     }
 }
---
+++ ExFixInterface/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace ExFixInterface.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
