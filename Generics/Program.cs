1ddef6c4764f6011339f9dfe8b871365ee4c46b8
dani <rosario.dani01@gmail.com>
2026-04-10 21:22:48 -0300

    Add Product entity and update program to find max product

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using Generics.Entities;
+using Generics.Services;
+
 
 namespace Generics
 {
@@ -10,6 +11,23 @@ namespace Generics
     {
         static void Main(string[] args)
         {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+            Product maxProduct = calculationService.Max(list);
+
+            Console.WriteLine("Most expensive product: " + maxProduct);
         }
     }
 }
